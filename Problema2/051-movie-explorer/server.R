4fde2c00154898beb675f7801bdd844e14e99cf9
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-07-29 23:35:07 -0300

    55%

--- Problema2/051-movie-explorer/server.R
+++ Problema2/051-movie-explorer/server.R
@@ -1,7 +1,6 @@
 library(ggplot2)
-library(ggvis)
 library(dplyr)
-  library(plotly)
+library(plotly)
 
 # Set up handles to database tables on app start
 dados <- read.csv("../../dados/ano-atual.csv")
@@ -11,35 +10,26 @@ dados.deputados <- dados %>%
   select(sgPartido, txNomeParlamentar, sgUF, vlrDocumento, txtDescricao, txtFornecedor)
 
 deputados.pb <- dados %>%
-  filter(sgUF == "PB") %>%
+  filter(sgUF == "PB" ) %>%
   select(txNomeParlamentar, sgPartido) %>%
   distinct()
 
-#filtrando os deputados da paraiba
-deputados.pb <- c("AGUINALDO RIBEIRO", "BENJAMIN MARANHÃO", "DAMIÃO FELICIANO", "EFRAIM FILHO", "HUGO MOTTA", "LUIZ COUTO", "MANOEL JUNIOR", "MARCONDES GADELHA", "PEDRO CUNHA LIMA", "RÔMULO GOUVEIA", "VENEZIANO VITAL DO RÊGO", "WELLINGTON ROBERTO", "WILSON FILHO")
 
-fornecedores.deputados.pb <- dados %>%
-  filter(txNomeParlamentar %in% deputados.pb ) %>%
-  select(txNomeParlamentar, txtCNPJCPF, txtFornecedor, txtDescricao, vlrDocumento) %>%
-  mutate(txtCNPJCPF = as.factor(txtCNPJCPF)) %>%
-  group_by(txNomeParlamentar, txtCNPJCPF, txtFornecedor, txtDescricao) %>%
-  summarise(valor.total = sum(vlrDocumento)) %>%
-  ungroup()
+#deputados.by.estado <- function(nome.estado, partido) {
+#    ifelse(partido == "TODOS",
+#      dados.deputados %>% filter(sgUF == nome.estado),
+#      dados.deputados %>% filter(sgUF == nome.estado) %>%  filter(sgPartido == partido))
+#    
+#}
 
-ggplot(data=fornecedores.deputados.pb,
-       aes(x=reorder(txNomeParlamentar, valor.total), y=valor.total),fill = sgPartido)+ 
-      geom_point(stat="identity") +
-       coord_flip()
+deputados.by.estado <- function(nome.estado, partido) {
+         dados.deputados %>% 
+          filter(sgUF == nome.estado)
 
-
-
-
-deputados.by.estado <- function(nome.estado) {
-  dados.deputados %>%
-    filter(sgUF == nome.estado)
-    
 }
 
+
+deputados.by.estado("PB", "TODOS")
 
 
 minx <- min(movies$rating)
@@ -54,18 +44,18 @@ shinyServer(function(input, output) {
     cor <- c("TODOS","DEM", "PCdoB","PDT","PEN", "PHS","PMB","PMDB","PP","PPS","PR","PRB",
               "PROS","PRP","PRTB","PSB", "PSC", "PSD", "PSDB", "PSL", "PSOL", "PT", 
               "PTB", "PTdoB", "PTN", "PV", "REDE", "SD", "S.PART.", "NA")
-    
+
+  
     # a simple histogram of movie ratings
-    p <- plot_ly(deputados.by.estado(input$region), 
+    p <- plot_ly(data = deputados.by.estado(input$region, input$partido),
                  x = txNomeParlamentar, 
                  y = vlrDocumento, 
                  mode = "markers", 
-                 text = paste("Fornecedor:", txtFornecedor),
+                 text = paste(sgPartido,
+                              "</br>Empresa:", txtFornecedor),
                  size = vlrDocumento
     )
     p
-    # style the xaxis
-    #layout(p, xaxis = list(title = "Ratings", range = c(minx, maxx), autorange = F,
-    #                      autotick = F, tick0 = minx, dtick = size))
+
   })
 })
