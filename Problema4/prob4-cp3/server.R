dd45af6cea7caf77d815027f0036b1279fc03443
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-09-21 21:05:11 -0300

    11% perguntas selecionadas

--- Problema4/prob4-cp3/server.R
+++ Problema4/prob4-cp3/server.R
@@ -5,6 +5,18 @@ library(dplyr)
 source("import_data.R")
 import_data()
 
+#Perguntas
+#Pergunta 3
+#A partir da classificação dos anos pelos gêneros dos filmes, conseguiríamos traçar uma linha cronológica de cada gênero. Mais especificamente, mostraríamos os altos de baixos de cada gênero.
+#Resposta 3
+#Acredito que conseguiremos plotar essa linha cronológica de cada gênero, mostrando os picos de cada um.
+
+#Pergunta 4
+#Analisando os outros dados fornecidos, verificando o ano, conseguiríamos dizer quais gêneros fizeram mais sucesso a partir da quantidade de público que eles tiveram?
+
+#Resposta 4
+#Essa é uma análise simples, acredito que conseguiremos montar um ranking dos gêneros mais populares a partir de cada ano.
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
