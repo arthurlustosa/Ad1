9f7a982d46c0b8a599186c12fea32aead2b86941
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-09-27 22:51:19 -0300

    markdown com explicação dos dados

--- Problema4/prob4-cp3/server.R
+++ Problema4/prob4-cp3/server.R
@@ -7,7 +7,16 @@ library(plotly)
 library(resample)
 
 source("import_data.R")
+
 import_data()
+
+movies.genre = read.csv("movie-genre.csv")
+filmes.ano = read.csv("filmes.ano.csv", stringsAsFactors = F)
+ratings = read.csv("ratings.csv")
+medianas.ano <- read.csv("medianas_filme.csv")
+filmes.ano = read.csv("filmes.ano.csv", stringsAsFactors = F)
+todos.generos.medianas <- read.csv("todos.generos.medianas.csv")
+dados.completo.filmes <- inner_join(filmes.ano, movies.genre, by="movieId")
 
 
 filmes.por.genero <- function(genero) {
@@ -15,6 +24,7 @@ filmes.por.genero <- function(genero) {
     filter(genre == genero) %>%
     group_by(ano) %>%
     summarise(mediana.ano = median(rating)) %>%
+    mutate(genre = genero) %>%
     distinct()
 }
 
@@ -22,7 +32,7 @@ dados.completo.filmes <- dados.completo.filmes %>%
   select(movieId, title.x, ano, genre, rating, popularity) 
 
 medianas.generos <- function(genre){
-  todos.generos.mediadas %>%
+  todos.generos.medianas %>%
     filter(genero == genre)
 }
 
@@ -30,9 +40,7 @@ medianas.generos <- function(genre){
 shinyServer(function(input, output) {
   
   output$trendPlot <- renderPlotly({
-    # size of the bins depend on the input 'bins'
-    #size <- (maxx - minx) / input$bins
-    
+
     minyear <- input$year[1]
     maxyear <- input$year[2]
     
@@ -43,10 +51,10 @@ shinyServer(function(input, output) {
     p <- plot_ly(dados, x = ano, y = mediana.ano)
     p %>%
       add_trace(y = fitted(loess(mediana.ano ~ as.numeric(ano))), x = ano) %>%
-      layout(title = "Median duration of unemployment (in weeks)",
-             showlegend = FALSE) %>%
+      layout(title = "Linha cronológica dos gêneros baseada na mediana das avaliações",
+             showlegend = F) %>%
       dplyr::filter(mediana.ano == max(mediana.ano)) %>%
-      layout(annotations = list(x = ano, y = mediana.ano, text = "Peak", showarrow = T))
+      layout(annotations = list(x = ano, y = mediana.ano, text = "Pico Max", showarrow = T))
   })
   
   output$medianaPlot <- renderPlot({
@@ -61,13 +69,40 @@ shinyServer(function(input, output) {
       ggplot(aes(x = ano, ymin = limite.inferior, ymax = limite.superior)) +
       geom_errorbar(width = 3) +
       scale_x_continuous(breaks = pretty(g$ano, n = 15)) +
-      labs(x="Ano do filme", y="Mediana")
+      labs(title="\nIntervalo de Confiança da \nMediana das Avaliações dos Gêneros por Ano\n", x="Ano", y="Mediana")
+    
+  })
+  
+  output$compare2genres <- renderPlotly({
+    
+    g1 <- filmes.por.genero(input$genre1)
+    g2 <- filmes.por.genero(input$genre2)
+    
+    if(input$genre1 != input$genre2){
+      g <- rbind(g1, g2)
+      
+      p <- plot_ly(g, x = ano, y = mediana.ano, group = genre, colour = genre)
+      p %>%
+        layout(title = "Comparação entre dois gêneros",
+               showlegend = TRUE)
+      
+    }else{
+      
+      p <- plot_ly(g1, x = ano, y = mediana.ano, group = genre, colour = genre)
+      p %>%
+        layout(title = "Comparação entre dois gêneros",
+               showlegend = TRUE) %>%
+        dplyr::filter(mediana.ano == max(mediana.ano))
+      
+    }
+    
+    
+    
+
     
   })
   
 })
-
-
 
 
 
--- Problema4/prob4-cp3/ui.R
+++ Problema4/prob4-cp3/ui.R
@@ -3,23 +3,61 @@ library(plotly)
 
 
 shinyUI(fluidPage(
-  titlePanel("Análise dos filmes por gênero entre 1902 e 2016"),
+  titlePanel("      Análise dos Gêneros Cinematográficos entre 1902 e 2016"),
   sidebarPanel(
-    selectInput("genre", "Genero (a movie can have multiple genres)",
+    h4("Os dados"),
+    p("Os dados são referentes a filmes e suas avaliações feitas por usuários entre os anos 1996 ate 2016. Os dataset possui filmes dos anos de 1902 até 2016."),
+    h4("Tratamento dos dados"),
+    p("Para realizarmos nossas visualizações, os dados iniciais precisaram ser tratados e os dataframes montados. Para entender o processo de tratamento dos dados clique no link abaixo."),
+    a("www.google.com"),
+    h4("Questão 1"),
+    p("Seria possível traçar uma linha cronológica de cada gênero mostrando os pontos máximos e então visualizarmos o comportamento de cada gênero nos respectivos anos.
+No primeiro gráfico conseguimos visualizar como os gêneros se comportam nos anos. Temos um traço (linha laranja) que normaliza a variação do comportamento dos valores. 
+"),
+    h4("Para iniciar a visualização:"),
+    h5("Escolha o gênero que você deseja visualizar a linha do tempo"),
+    selectInput("genre", "Gênero",
                 c("Action", "Adventure", "Animation","Children", "Comedy", "Crime", 
                   "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "IMAX", 
                   "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller","War","Western")
     ),
-    sliderInput("year", "Anos", 1902, 2016, value = c(1902, 2016))
+    h5("Escolha um intervalo de tempo entre os anos abaixo"),
+    sliderInput("year", "Anos", 1902, 2016, value = c(1902, 2016)),
+    p("Na segunda visualização, propomos exibir os intervalos de confiança medidos a partir da mediana de cada gênero no ano correspondente. Essa estratégia visa agregar mais confiança ao gráfico mostrado acima, visto que, ao anasilarmos os intervalos de confiança conseguimos afirmar, com mais segurança, os resultados obtidos."),
+    
+    p(" "),
+    p(" "),
+    p(" "),
+    h4("Questão 2"),
+    p("Ao comparar o comportamento de dois gêneros conseguimos inferir qual dos dois obteve um melhor comportamento? 
+A partir da validação dos resultados obtivos na primeira questão pelo gráfico dos intervalos de confiança, conseguimos observar com clareza o comportamento de dois gêneros no decorrer dos anos. Podemos ver quais tiveram um crescimento ou queda."),
+    selectInput("genre1", "Gênero 1",
+                c("Action", "Adventure", "Animation","Children", "Comedy", "Crime", 
+                  "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "IMAX", 
+                  "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller","War","Western")
+    ),
+    selectInput("genre2", "Gênero 2",
+                c("Comedy", "Adventure", "Action", "Animation","Children", "Crime", 
+                  "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "IMAX", 
+                  "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller","War","Western")
+    ),
+    h4("Resultados"),
+    p("Os resultados obtidos nas tentativas de resposta das duas questões foram bastante satisfatórios. Conseguimos observar de forma clara o comportamento dos gêneros cinematográficos no decorrer dos anos. Porém ao tentar responder as questões mostradas acima chegamos a outros questionamentos: Seria possível responder quais fatores influenciaram o crescimento ou decaimento de determinado gênero? Quais gêneros possuem mais correlação e quais não possuem?"),
+    p("Acreditamos que essas questões podem ser respondidas no futuro.")
+    
     
     
   ),
   mainPanel(
-    p("A proposta dessa visualização é mostrar uma linha cronológica de cada gênero, mostrando os pontos máximos e mínimos no decorrer do ano."),
-    p(),
+    p("\n"),
     plotlyOutput("trendPlot"),
-    p("A segunda visualização nos mostra os intervalos de confiança calculados a partir da mediana das notas de cada gênero por ano. "),
-    plotOutput("medianaPlot")
+    p("\n"),
+    plotOutput("medianaPlot"),
+    p("\n"),
+    p("\n"),
+    p("\n"),
+    p("\n"),
+    plotlyOutput("compare2genres")
   )
 ))
 
